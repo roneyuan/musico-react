f1ba6134f923bd5225bfd1d1c5d72a2ea7090489
roneyuan <roneyuan_m03@hotmail.com>
2017-06-06 16:28:20 -0400

    Initial setup for React

--- views/js/index.js
+++ views/js/index.js
@@ -1,2 +1,8 @@
-require('babel-polyfill');
+import React from 'react';
+import ReactDOM  from 'react-dom';
 
+import Person from './components/person';
+
+document.addEventListener('DOMContentLoaded', () =>
+    ReactDOM.render(<Person />, document.getElementById('app'))
+); 
