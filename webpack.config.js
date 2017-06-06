f1569e9ba2903b6668bdce4db8a069e7dae7f38c
roneyuan <roneyuan_m03@hotmail.com>
2017-06-06 13:16:17 -0400

    Project setup for React

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'js/index.js'),
+    entry: path.resolve(__dirname, 'views/js/index.js'),
     output: {
-        path: path.resolve(__dirname, 'build/js'),
+        path: path.resolve(__dirname, 'views/build/js'),
         filename: 'index.js',
     },
     devtool: 'inline-source-map',
