7acb76d2aa5165c8a3c9fed0b5118e08de8b9850
roneyuan <roneyuan_m03@hotmail.com>
2017-06-17 15:53:22 -0400

    Issue fixed: Cannot read property getCurrentLocation for react router

--- views/js/index.js
+++ views/js/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
 import EventList from './components/event-list';
+import PostEvent from './components/post-event';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import Event from './components/event';
 import store from './store';
+import {Router, Route, hashHistory} from 'react-router';
+
+const routes = (
+	<Provider store={store}>
+		<Router histroy={hashHistory}>
+			<Route path="/events" component={EventList} />
+			<Route path="/postevents" component={PostEvent} />		
+		</Router>
+	</Provider>
+)
+
 
 
 document.addEventListener('DOMContentLoaded', () =>
   ReactDOM.render(
   <Provider store={store}>
-  	<EventList />
-  </Provider>,
+		<Router history={hashHistory}>
+			<Route path="/events" component={EventList} />
+			<Route path="/postevents" component={PostEvent} />		
+		</Router>
+	</Provider>,
   document.getElementById('app'))
 ); 
