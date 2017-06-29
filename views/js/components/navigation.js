124b9af42d8efdd210d0dd7341d31e2be782ed06
roneyuan <roneyuan_m03@hotmail.com>
2017-06-28 21:58:15 -0400

    Add active style for link

--- views/js/components/navigation.js
+++ views/js/components/navigation.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 
 export default function Navigation(props) {
   return (
     <div className="app-navbar">
       <ul>
-        <li className="brand"><a href="/">Musico</a></li>
-        <li className="menu"><a href="/#profile">Profile</a></li>
-        <li className="menu"><a href="/#postevents">Post</a></li>
-        <li className="menu"><a href="/#events">Events</a></li>
+        <li className="brand"><Link to="/">Musico</Link></li>
+        <li className="menu"><Link activeStyle={{ color: 'red' }} to="/profile">Profile</Link></li>
+        <li className="menu"><Link activeStyle={{ color: 'red' }} to="/postevents" >Post</Link></li>
+        <li className="menu"><Link activeStyle={{ color: 'red' }} to="/events" >Events</Link></li>
       </ul>
     </div>
   );
