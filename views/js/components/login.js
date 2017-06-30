39e6a43960f73ab2e7c104dfcf9c63ba308a945c
roneyuan <roneyuan_m03@hotmail.com>
2017-06-30 13:31:14 -0400

    Add buttons

--- views/js/components/login.js
+++ views/js/components/login.js
@@ -7,6 +7,14 @@ export default function Login() {
       <button className="google-button">
       	<a href="api/user/auth/google">Login with Google</a>
       </button>
+
+      <button className="demo-button">
+      	<a href="">Explore with Demo Account</a>
+      </button>
+
+      <button className="register-button">
+      	<a href="/#/register">Register</a>      
+      </button>
     </div>
   )
 }
