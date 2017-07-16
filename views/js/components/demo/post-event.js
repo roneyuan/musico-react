ca038710dc9377b9c47a58fe8348e33450b66a65
roneyuan <roneyuan_m03@hotmail.com>
2017-07-16 18:10:17 -0400

    Add date input and minor fix for refreshing the form

--- views/js/components/demo/post-event.js
+++ views/js/components/demo/post-event.js
@@ -2,74 +2,88 @@ import React , { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { demoPostEvent, newPostForm } from '../../actions/demo/index';
-// import { browserHistory } from 'react-router';
 
 
 class PostEvent extends Component {
+  componentWillMount() {
+    this.props.newPostForm();
+  }
+
   render() {
     let content;
-    console.log(this.props.events)
+
     if (this.props.events.length === 0) {
-      content = <div className="content__post-event">
-        <form onSubmit={(event) => {
-          event.preventDefault();
+      content = (
+        <div className="content__post-event">
+          <form onSubmit={(event) => {
+            event.preventDefault();
 
-          let name = event.target.name.value;
-          let description = event.target.description.value;
-          let price = event.target.price.value;
-          let tag = event.target.tag.value;
-          let location = event.target.location.value;
-          
+            let name = event.target.name.value;
+            let description = event.target.description.value;
+            let price = event.target.price.value;
+            let tag = event.target.tag.value;
+            let location = event.target.location.value;
+            
+            this.props.demoPostEvent(name, price, description, location, tag);
+          }}>
 
-          this.props.demoPostEvent(name, price, description, location, tag);
-
-          // browserHistory.push('/app/events');
-        }}>
-
-        <div className="form-control">
-          <label> Name: </label>
-          <input type="text" name="name" />
+          <div className="form-control">
+            <label> Name: </label>
+            <input type="text" name="name" />
+          </div>
+          <div className="form-control">
+            <label> Location: </label>
+            <input type="text" name="location" />
+          </div>
+          <div className="form-control">
+            <label> Date: </label>
+            <input type="datetime-local" name="date" />
+          </div>          
+          <div className="form-control">
+            <label> Price: </label>
+            <input type="text" name="price" />
+          </div>
+          <div className="form-control">
+            <label> Description: </label>
+            <textarea type="text" name="description"></textarea>
+          </div>
+          <div className="form-control">
+            <label> Tag: </label>
+            <input type="text" name="tag" /> 
+          </div>
+          <div className="form-control">                            
+            <button className="btn__submit">Submit</button>
+          </div>
+          </form>
         </div>
-        <div className="form-control">
-          <label> Location: </label>
-          <input type="text" name="location" />
-        </div>
-        <div className="form-control">
-          <label> Price: </label>
-          <input type="text" name="price" />
-        </div>
-        <div className="form-control">
-          <label> Description: </label>
-          <textarea type="text" name="description"></textarea>
-        </div>
-        <div className="form-control">
-          <label> Tag: </label>
-          <input type="text" name="tag" /> 
-        </div>
-        <div className="form-control">                            
-          <button className="btn__submit">Submit</button>
-        </div>
-        </form>
-      </div>
+      );
     } else {
-      content = <div>POSTED <button onClick={() => this.props.newPostForm()} className="btn___submit"> Create </button></div>;
+      content = (
+        <div className="content__event-posted">
+          <div>The event is posted. If you would like to create another event, please click the button below.</div>
+          <div>
+            <button 
+              onClick={ () => this.props.newPostForm() } 
+              className="btn__refresh"> 
+              New Event 
+            </button>
+          </div>
+          
+        </div>
+      );
     }
 
-    console.log(content)
-    return (content
-
-  )}
+    return content;
+  }
 }
 
 function mapStateToProps(state) {
-  console.log(state);
   return {
     events: state.eventsDatabase.events
   }
 }
 
 function matchDispatchToProps(dispatch) {
-  // console.log("DIS", dispatch)
   return bindActionCreators({ demoPostEvent, newPostForm }, dispatch)
 }
 
