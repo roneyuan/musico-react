0f9bdd476b9897bd30135ea63034dc98023ee476
roneyuan <roneyuan_m03@hotmail.com>
2017-08-05 20:08:04 -0400

    Minor fix

--- views/js/reducers/index.js
+++ views/js/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import CommentReducer from './comment-reducer';
 import EventDatabase from './events-database';
-import PostedEvent from './posted-event';
 import UserDatabase from './user-database';
 
 
