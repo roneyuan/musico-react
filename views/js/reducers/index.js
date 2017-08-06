34fa4ef5944db35feac8da9c48e3fe7a2b45e133
roneyuan <roneyuan_m03@hotmail.com>
2017-08-05 20:18:51 -0400

    Quick fix

--- views/js/reducers/index.js
+++ views/js/reducers/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import CommentReducer from './comment-reducer';
 import EventDatabase from './events-database';
+import PostedEvent from './posted-event';
 import UserDatabase from './user-database';
 
 
 const AllReducers = combineReducers({
 	commentReducer: CommentReducer,
 	eventsDatabase: EventDatabase,
+	postedEvent: PostedEvent,
 	userDatabase: UserDatabase
 });
 
